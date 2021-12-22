9f20e1aad4d34e8a028a9ee2567ce9fe641361fd
杨子晗 <yangzihan2000@outlook.com>
2021-12-22 19:58:57 +0800

    class shader v1

--- src/shader.cpp
+++ src/shader.cpp
@@ -4,6 +4,27 @@
 #include <iostream>
 #include <streambuf>
 
+#include <exception>
+
+std::string type2str(GLuint ID)
+{
+    switch (ID)
+    {
+    case GL_VERTEX_SHADER:
+        return "Vertex Shader";
+        break;
+    case GL_FRAGMENT_SHADER:
+        return "Fragment Shader";
+        break;
+    case GL_GEOMETRY_SHADER:
+        return "Geometry Shader";
+        break;
+    default:
+        return "Unknown Shader";
+        break;
+    }
+}
+
 Shader::Shader(const std::string &filePath, unsigned int type) : type(type)
 {
     std::ifstream shaderFile(filePath);
@@ -13,5 +34,51 @@ Shader::Shader(const std::string &filePath, unsigned int type) : type(type)
     ID = glCreateShader(type);
     glShaderSource(ID, 1, &codeStr, NULL);
     glCompileShader(ID);
-    
+}
+
+void Shader::checkError()
+{
+    GLint success;
+    GLchar *infoLog;
+    glGetShaderiv(ID, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        infoLog = new GLchar[4096];
+        glGetShaderInfoLog(ID, 4096, NULL, infoLog);
+        std::cerr << "Error of " << type2str(ID) << std::endl;
+        std::cerr << infoLog << std::endl;
+        throw std::exception();
+    }
+}
+
+ShaderProgram::ShaderProgram(const Shader &vertexShader, const Shader &fragmentShader)
+{
+    ID = glCreateProgram();
+    glAttachShader(ID, vertexShader.getID());
+    glAttachShader(ID, fragmentShader.getID());
+    glLinkProgram(ID);
+}
+
+ShaderProgram::ShaderProgram(const Shader &vertexShader, const Shader &fragmentShader, const Shader &geometryShader)
+{
+    ID = glCreateProgram();
+    glAttachShader(ID, vertexShader.getID());
+    glAttachShader(ID, geometryShader.getID());
+    glAttachShader(ID, fragmentShader.getID());
+    glLinkProgram(ID);
+}
+
+void ShaderProgram::checkError()
+{
+    GLint success;
+    GLchar *infoLog;
+    glGetShaderiv(ID, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        infoLog = new GLchar[4096];
+        glGetProgramInfoLog(ID, 4096, NULL, infoLog);
+        std::cerr << "Error of Program" << std::endl;
+        std::cerr << infoLog << std::endl;
+        throw std::exception();
+    }
 }
