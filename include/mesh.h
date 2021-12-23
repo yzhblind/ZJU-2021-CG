41a6dcdde27e953b1daa62a3c1d1a0efc80a8580
杨子晗 <yangzihan2000@outlook.com>
2021-12-24 01:47:45 +0800

    class mesh v1

--- include/mesh.h
+++ include/mesh.h
@@ -1,1 +1,45 @@
 #pragma once
+
+#include <glad/glad.h> 
+
+#include <glm/glm.hpp>
+
+#include <shader.h>
+#include <texture.h>
+
+#include <string>
+#include <vector>
+
+struct Vertex
+{
+    glm::vec3 pos;
+    glm::vec3 normal;
+    glm::vec2 texCoords;
+    glm::vec3 tan;
+    glm::vec3 biTan;
+};
+
+class Mesh
+{
+public:
+    Mesh() : VAO(0), VBO(0), EBO(0) { vertices.clear(), indices.clear(), textures.clear(); }
+    void init();
+    void draw(ShaderProgram &prgm);
+    void push_back(const Vertex &e) { vertices.push_back(e); }
+    void push_back(const unsigned int &e) { indices.push_back(e); }
+    void push_back(const Texture &e) { textures.push_back(e); }
+
+private:
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+    unsigned int VAO, VBO, EBO;
+
+    void createObject();
+    void sendData();
+    void setAttribute();
+
+    void setTexture(ShaderProgram &prgm);
+    void drawCall();
+    void recover();
+};
--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,1 +1,87 @@
-#include "mesh.h"
+#include <mesh.h>
+
+using namespace glm;
+using namespace std;
+
+void Mesh::createObject()
+{
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+}
+void Mesh::sendData()
+{
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+}
+void Mesh::setAttribute()
+{
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, normal));
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, texCoords));
+    glEnableVertexAttribArray(3);
+    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, tan));
+    glEnableVertexAttribArray(4);
+    glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, biTan));
+}
+
+void Mesh::init()
+{
+    createObject();
+    sendData();
+    setAttribute();
+    recover();
+}
+
+void Mesh::setTexture(ShaderProgram &prgm)
+{
+    unsigned int diffuseCnt = 0;
+    unsigned int specularCnt = 0;
+    unsigned int normalCnt = 0;
+    unsigned int heightCnt = 0;
+    string name;
+    for (unsigned int i = 0; i < textures.size(); ++i)
+    {
+        glActiveTexture(GL_TEXTURE0 + i);
+        switch (textures[i].type)
+        {
+        case DIFFUSE_T:
+            name = "diffuse_texture" + to_string(diffuseCnt++);
+            break;
+        case SPECULAR_T:
+            name = "specular_texture" + to_string(specularCnt++);
+            break;
+        case NORMAL_T:
+            name = "normal_texture" + to_string(normalCnt++);
+            break;
+        case HEIGHT_T:
+            name = "height_texture" + to_string(heightCnt++);
+            break;
+        }
+        prgm.setInt(name, i);
+        glBindTexture(GL_TEXTURE_2D, textures[i].id);
+    }
+}
+void Mesh::drawCall()
+{
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+}
+void Mesh::recover()
+{
+    glBindVertexArray(0);
+    glActiveTexture(GL_TEXTURE0);
+}
+
+void Mesh::draw(ShaderProgram &prgm)
+{
+    setTexture(prgm);
+    drawCall();
+    recover();
+}
