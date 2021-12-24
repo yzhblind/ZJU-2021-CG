7ec039c21ff53def12ab39c86113dd2c0a58aafa
杨子晗 <yangzihan2000@outlook.com>
2021-12-25 00:14:42 +0800

    init light struct

--- include/light.h
+++ include/light.h
@@ -1,1 +1,44 @@
 #pragma once
+
+#include <glad/glad.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <shader.h>
+
+struct dirLight
+{
+    glm::vec3 direction;
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+};
+
+struct pointLight
+{
+    glm::vec3 position;
+
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+struct spotLight
+{
+    glm::vec3 position;
+    glm::vec3 direction;
+    float cutOff;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+void useLight(ShaderProgram &prgm, const dirLight &dir);
+void useLight(ShaderProgram &prgm, const pointLight &point);
+void useLight(ShaderProgram &prgm, const spotLight &spot);
--- src/light.cpp
+++ src/light.cpp
@@ -1,1 +1,14 @@
 #include <light.h>
+
+void useLight(ShaderProgram &prgm, const dirLight &dir)
+{
+    
+}
+void useLight(ShaderProgram &prgm, const pointLight &point)
+{
+
+}
+void useLight(ShaderProgram &prgm, const spotLight &spot)
+{
+
+}
