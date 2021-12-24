adbc404b91b1b0f6eee82b80412671e21fc36bb3
杨子晗 <yangzihan2000@outlook.com>
2021-12-25 01:43:36 +0800

    matrix stack v1

--- include/myUtils.h
+++ include/myUtils.h
@@ -4,9 +4,25 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
+#include <vector>
 
 class MatrixStack
 {
+public:
+    MatrixStack() { reset(); }
+    void reset();
+    void push() { modelStack.push_back(modelStack.back()), normalStack.push_back(normalStack.back()); }
+    void pop() { modelStack.pop_back(), normalStack.pop_back(); }
+    glm::mat4 getModelMatrix() { return modelStack.back(); }
+    glm::mat3 getNormalMatrix() { return normalStack.back(); }
+    void rotate(float angle, glm::vec3 axis);
+    void scale(glm::vec3 factor);
+    void translate(glm::vec3 direction);
+    void mul(const glm::mat4 &trans);
 
+private:
+    std::vector<glm::mat4> modelStack;
+    std::vector<glm::mat3> normalStack;
 };
--- src/myUtils.cpp
+++ src/myUtils.cpp
@@ -1,1 +1,37 @@
 #include <myUtils.h>
+
+using namespace glm;
+using namespace std;
+
+inline mat3 calcNormal(const mat4 &t) { return mat3(transpose(inverse(t))); }
+
+void MatrixStack::reset()
+{
+    modelStack.clear(), normalStack.clear();
+    modelStack.push_back(mat4(1.0f));
+    normalStack.push_back(mat3(1.0f));
+}
+void MatrixStack::rotate(float angle, vec3 axis)
+{
+    mat4 &t = modelStack.back();
+    t = glm::rotate(t, radians(angle), axis);
+    normalStack.back() = calcNormal(t);
+}
+void MatrixStack::scale(vec3 factor)
+{
+    mat4 &t = modelStack.back();
+    t = glm::scale(t, factor);
+    normalStack.back() = calcNormal(t);
+}
+void MatrixStack::translate(glm::vec3 direction)
+{
+    mat4 &t = modelStack.back();
+    t = glm::translate(t, direction);
+    normalStack.back() = calcNormal(t);
+}
+void MatrixStack::mul(const mat4 &trans)
+{
+    mat4 &t = modelStack.back();
+    t = trans * t;
+    normalStack.back() = calcNormal(t);
+}
