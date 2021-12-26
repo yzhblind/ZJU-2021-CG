1267dfe07b9076203a8312cf1305a3a9f8733b17
mengbierr <mengbierr@gmail.com>
2021-12-26 21:40:57 +0800

    add trans to basic mesh

--- include/basic.h
+++ include/basic.h
@@ -5,14 +5,14 @@
 class Cube : public Mesh
 {
 public:
-	Cube(GLfloat len);
+	Cube(GLfloat len, glm::vec3 trans=glm::vec3(0,0,0));
 private:
 	GLfloat length;
 };
 class Sphere :public Mesh
 {
 public:
-	Sphere(GLfloat r, int a=100, int b=100);
+	Sphere(GLfloat r, int a=100, int b=100, glm::vec3 trans = glm::vec3(0, 0, 0));
 private:
 	GLfloat r;
 	int a, b;
@@ -21,7 +21,7 @@ class Prism :public Mesh
 {
 public:
 	//棱柱，r:半径，h:高度，num:棱数
-	Prism(GLfloat r, GLfloat h, GLint num);
+	Prism(GLfloat r, GLfloat h, GLint num, glm::vec3 trans = glm::vec3(0, 0, 0));
 private:
 	GLfloat r, h;
 	GLint num;
@@ -29,12 +29,12 @@ private:
 class Cylinder :public Prism
 {
 public:
-	Cylinder(GLfloat r, GLfloat h) :Prism(r, h, 100) {};
+	Cylinder(GLfloat r, GLfloat h, glm::vec3 trans = glm::vec3(0, 0, 0)) :Prism(r, h, 100,trans) {};
 };
 class Cone : public Mesh
 {
 public:
-	Cone(GLfloat r, GLfloat h, int num=100);
+	Cone(GLfloat r, GLfloat h, int num=100, glm::vec3 trans = glm::vec3(0, 0, 0));
 private:
 	GLfloat r, h;
 };
@@ -42,7 +42,7 @@ class Frustum :public Mesh
 {
 public:
 	//棱台 rh:上面半径，rl：下面半径，h：高
-	Frustum(GLfloat rh, GLfloat rl, GLfloat h, int num );
+	Frustum(GLfloat rh, GLfloat rl, GLfloat h, int num, glm::vec3 trans = glm::vec3(0, 0, 0));
 private:
 	GLfloat rh, rl, h;
 	int num;
