edc3a4f4963de8567df03773b7736b3a9ee8a817
mengbierr <mengbierr@gmail.com>
2021-12-26 21:18:14 +0800

    add some interactions

--- src/main.cpp
+++ src/main.cpp
@@ -79,6 +79,7 @@ void mouse_callback(GLFWwindow *window, double xpos, double ypos)
 }
 void scroll_callback(GLFWwindow *window, double xoffset, double yoffset)
 {
+    myGame.zoomInOutCamera(yoffset);
 }
 void mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
 {
@@ -104,5 +105,11 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
         else
             glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
         cursorEnabled = !cursorEnabled;
+    }
+    if (key == GLFW_KEY_V && action == GLFW_PRESS)
+    {
+        glm::vec3 cameraPos=myGame.getCameraPos(), cameraFront=myGame.getCameraFront();
+        printf("Camera at (%.3lf,%.3lf,%.3lf)\n front (%.3lf, %.3lf,%.3lf)", cameraPos.x, cameraPos.y, cameraPos.z, cameraFront.x, cameraFront.y, cameraFront.z);
+
     }
 }
